8ba08b1bed0a55dad702562fea339b6a46695b39
Shah Darshil <darshilshah15575@gmail.com>
2023-03-04 21:22:25 +0530

    Added the AI vs Human text written analysis algorithm and added another upload.html file

---
+++ models/model_ai_human.py
@@ -0,0 +1,28 @@
+from flask import jsonify
+import requests
+
+from keys import keys;
+
+def get_ai_vs_human_response(payload):
+	API_URL = keys.API_URL
+	headers = keys.headers
+
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+
+
+def calculate_ai_vs_human_score(input_text):
+	output = get_ai_vs_human_response({
+		"inputs": input_text,
+	})
+
+	human_score = next((item for item in output[0] if item["label"] == "Human"), None)
+	chatgpt_score = next((item for item in output[0] if item["label"] == "ChatGPT"), None)
+	h_score = human_score["score"]
+	chat_score = chatgpt_score["score"]
+        #print("h_score:", h_score, type(h_score))
+        #print("chat_score:", chat_score, type(chat_score))
+        #h_score_new = float(round(float(h_score), 2))
+        #chat_score_new = float(round(float(chat_score), 2))
+	return { 'ai': chat_score, 'human': h_score }
+
